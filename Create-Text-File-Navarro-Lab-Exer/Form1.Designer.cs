d683e379c8012350295d6e9f83f12c6a8074189b
ZakurOwO <johndanielnavarro130@gmail.com>
2025-10-15 18:32:43 +0800

    Created two winform (no design yet)

--- Create-Text-File-Navarro-Lab-Exer/Form1.Designer.cs
+++ Create-Text-File-Navarro-Lab-Exer/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtinput = new System.Windows.Forms.RichTextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtinput
+            // 
+            this.txtinput.Location = new System.Drawing.Point(12, 61);
+            this.txtinput.Name = "txtinput";
+            this.txtinput.Size = new System.Drawing.Size(378, 103);
+            this.txtinput.TabIndex = 0;
+            this.txtinput.Text = "";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCreate.Location = new System.Drawing.Point(157, 216);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(84, 41);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 374);
+            this.ClientSize = new System.Drawing.Size(402, 269);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtinput);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +64,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox txtinput;
+        private System.Windows.Forms.Button btnCreate;
     }
 }
 
---
+++ Create-Text-File-Navarro-Lab-Exer/Form2.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Create_Text_File_Navarro_Lab_Exer
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOkay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(12, 101);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(288, 84);
+            this.txtFileName.TabIndex = 0;
+            this.txtFileName.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter File Name";
+            // 
+            // btnOkay
+            // 
+            this.btnOkay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnOkay.Location = new System.Drawing.Point(91, 228);
+            this.btnOkay.Name = "btnOkay";
+            this.btnOkay.Size = new System.Drawing.Size(120, 45);
+            this.btnOkay.TabIndex = 2;
+            this.btnOkay.Text = "Okay";
+            this.btnOkay.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 285);
+            this.Controls.Add(this.btnOkay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOkay;
+    }
+}
