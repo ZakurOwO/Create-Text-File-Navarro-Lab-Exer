c301b24af9828c18543fc817a376933f3f29546e
ZakurOwO <johndanielnavarro130@gmail.com>
2025-10-15 18:24:59 +0800

    Add project files.

---
+++ Create-Text-File-Navarro-Lab-Exer/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Create_Text_File_Navarro_Lab_Exer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
