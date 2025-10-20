85f3ba1f2621450652081e913a9edbb08f8ca762
ZakurOwO <johndanielnavarro130@gmail.com>
2025-10-20 19:20:12 +0800

    Added Frm Student Record

--- Create-Text-File-Navarro-Lab-Exer/FrmOpenTextFile.Designer.cs
+++ Create-Text-File-Navarro-Lab-Exer/FrmOpenTextFile.Designer.cs
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LV = new System.Windows.Forms.ListView();
+            this.lvShowText = new System.Windows.Forms.ListView();
             this.btnOpen = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
-            // LV
+            // lvShowText
             // 
-            this.LV.HideSelection = false;
-            this.LV.Location = new System.Drawing.Point(12, 30);
-            this.LV.Name = "LV";
-            this.LV.Size = new System.Drawing.Size(592, 223);
-            this.LV.TabIndex = 0;
-            this.LV.UseCompatibleStateImageBehavior = false;
+            this.lvShowText.AutoArrange = false;
+            this.lvShowText.HideSelection = false;
+            this.lvShowText.LabelWrap = false;
+            this.lvShowText.Location = new System.Drawing.Point(12, 30);
+            this.lvShowText.Name = "lvShowText";
+            this.lvShowText.Size = new System.Drawing.Size(592, 223);
+            this.lvShowText.TabIndex = 0;
+            this.lvShowText.UseCompatibleStateImageBehavior = false;
             // 
             // btnOpen
             // 
@@ -50,6 +52,7 @@
             this.btnOpen.TabIndex = 1;
             this.btnOpen.Text = "Open";
             this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // openFileDialog1
             // 
@@ -61,7 +64,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(616, 332);
             this.Controls.Add(this.btnOpen);
-            this.Controls.Add(this.LV);
+            this.Controls.Add(this.lvShowText);
             this.Name = "FrmOpenTextFile";
             this.Text = "FrmOpenTextFile";
             this.ResumeLayout(false);
@@ -70,7 +73,7 @@
 
         #endregion
 
-        private System.Windows.Forms.ListView LV;
+        private System.Windows.Forms.ListView lvShowText;
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
--- Create-Text-File-Navarro-Lab-Exer/FrmOpenTextFile.cs
+++ Create-Text-File-Navarro-Lab-Exer/FrmOpenTextFile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,36 @@ namespace Create_Text_File_Navarro_Lab_Exer
         public FrmOpenTextFile()
         {
             InitializeComponent();
+        }
+
+        private void DisplayToList()
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                lvShowText.Items.Clear();
+
+                using (StreamReader streamReader = File.OpenText(path))
+                {
+                    string line = "";
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        lvShowText.Items.Add(new ListViewItem(line));
+                    }
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            //DisplayToList();
+
+            FrmStudentRecord new frm1 = new FrmStudentRecord();
         }
     }
 }
